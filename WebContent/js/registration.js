be2d16901654eaffab85967b86ae9ec55d90ede2
jhfan <jhuafan@gmail.com>
2014-11-26 15:50:17 -0500

    15:49 11/26/2014
    add password verifird

--- WebContent/js/registration.js
+++ WebContent/js/registration.js
@@ -122,3 +122,34 @@ app.controller('DatepickerDemoCtrl', function ($scope) {
     $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
     $scope.format = $scope.formats[0];
   });
+app.directive("passwordVerify", function() {
+	   return {
+	      require: "ngModel",
+	      scope: {
+	        passwordVerify: '='
+	      },
+	      link: function(scope, element, attrs, ctrl) {
+	        scope.$watch(function() {
+	            var combined;
+
+	            if (scope.passwordVerify || ctrl.$viewValue) {
+	               combined = scope.passwordVerify + '_' + ctrl.$viewValue; 
+	            }                    
+	            return combined;
+	        }, function(value) {
+	            if (value) {
+	                ctrl.$parsers.unshift(function(viewValue) {
+	                    var origin = scope.passwordVerify;
+	                    if (origin !== viewValue) {
+	                        ctrl.$setValidity("passwordVerify", false);
+	                        return undefined;
+	                    } else {
+	                        ctrl.$setValidity("passwordVerify", true);
+	                        return viewValue;
+	                    }
+	                });
+	            }
+	        });
+	     }
+	   };
+	});
